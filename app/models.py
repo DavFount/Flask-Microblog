d62814c67c3d591127bf000d08d5fdd1c3734547
David Fountain <davfount@gmail.com>
2020-07-28 18:14:55 -0700

    Chapter 10 - Email Support & Resetting Password with Json Web Tokens

--- app/models.py
+++ app/models.py
@@ -1,8 +1,11 @@
 from datetime import datetime
 from hashlib import md5
-from werkzeug.security import generate_password_hash, check_password_hash
-from app import db, login
+from time import time
 from flask_login import UserMixin
+from werkzeug.security import generate_password_hash, check_password_hash
+import jwt
+from app import app, db, login
+
 
 @login.user_loader
 def load_user(id):
@@ -58,6 +61,20 @@ class User(UserMixin, db.Model):
             followers, (followers.c.followed_id == Post.user_id)).filter(
                 followers.c.follower_id == self.id)
         return followed.union(self.posts).order_by(Post.timestamp.desc())
+
+    def get_reset_password_token(self, expires_in=600):
+        return jwt.encode(
+            {'reset_password': self.id, 'exp': time() + expires_in},
+            app.config['SECRET_KEY'], algorithm='HS256').decode('utf-8')
+
+    @staticmethod
+    def verify_reset_password_token(token):
+        try:
+            id = jwt.decode(token, app.config['SECRET_KEY'],
+                            algorithms=['HS256'])['reset_password']
+        except:
+            return
+        return User.query.get(id)
         
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
